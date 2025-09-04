8e4360338213b43d95d221ad78662924fcc68173
Bruneizer <carlosbruno3856@gmail.com>
2025-09-04 15:42:12 +0200

    casi terminador

--- logica.js
+++ logica.js
@@ -1,18 +1,18 @@
-function FizzBuzz(limite, fizz_num, buzz_num){
-    let resultado = [];
+function fizzBuzz2(limit, fizz_num, buzz_num) {
+    let result = [];
+    for (let i = 1; i <= limit; i++) {
+        if (i % fizz_num === 0 && i % buzz_num === 0) {
+            result.push("FizzBuzz");
+        } else if (i % fizz_num === 0) {
+            result.push("Fizz");
+        } else if (i % buzz_num === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i);
+        }
+    }
+    return result.join(",");
 }
-for(let i = 1; i <= limite; i++ ){
-    if(i % fizz_num === 0 && i % buzz_num === 0){
-        resultado.push("FizzBuzz")
-    }
-    else if(i % fizz_num === 0){
-        resultado.push("Fizz")
-    }
-    else if(i % buzz_num === 0 ){
-        resultado.push("Buzz")
-    }
-    else(resultado.push(i));{
-    }
 
-}
-console.log(FizzBuzz(1000, 10, 30));
+// Ejemplo de uso:
+console.log(fizzBuzz2(20, 3, 5)); // Cuenta hasta 20, usando 3 y 5 como múltiplos
